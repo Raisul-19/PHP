ed7c66fded12a1cc6ee6352829fbe79017341d91
Raisul-19 <raisul15-14119@diu.edu.bd>
2024-05-31 07:08:28 +0600

    class 13 review

---
+++ Class-13 review/register-post.php
@@ -0,0 +1,131 @@
+<?php
+
+    require 'db.php';
+
+?>
+
+
+
+<?php
+
+    $name = $_POST['fname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $re_password = $_POST['re_password'];
+
+    $gender = $_POST['gender'];
+
+
+    // $upper = preg_match('@[A-Z]@', $password);
+    // $lower = preg_match('@[a-z]@', $password);
+    // $number = preg_match('@[0-9]@', $password);
+    // $splcr = preg_match('@[#, $, &, *]@', $password);
+
+
+
+
+
+    if(empty($name)){
+        $err_msg = 'Please write your name !';
+
+        header('location:register.php?name_error='.$err_msg);
+    }
+
+    
+    else if(empty($email)){
+        $err_msg = 'Please write your email !';
+
+        header('location:register.php?email_error='.$err_msg);
+    }
+
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $err_msg = 'please write correct email formate';
+
+        header('location:register.php?email_error='.$err_msg);
+    }
+
+    else if(empty($password)){
+        $err_msg = 'Please give a password !';
+
+        header('location:register.php?pass_error='.$err_msg);
+    }
+    
+    // else if(!$upper || !$lower || !$number || !$splcr || strlen($password) < 8){
+    //     $err_msg = 'give a strong password';
+
+    //     header('location:index.php?pass_error='.$err_msg);
+
+    // }
+
+    else if(empty($re_password)){
+        $err_msg = 'Confirm your password';
+
+        header('location:register.php?re_pass_error='.$err_msg);
+
+    }
+
+    else if($re_password != $password){
+        $err_msg = "your password didn't match";
+
+        header('location:register.php?re_pass_error='.$err_msg);
+
+    }
+
+
+    else if(empty($gender)){
+        $err_msg = "Select your Gender";
+
+        header('location:register.php?gender_error='.$err_msg);
+
+    }
+
+
+
+
+
+    else{
+
+        $uploded_file = $_FILES['photo'];
+        $after_explode = explode('.', $uploded_file['name']);
+        $extension = end($after_explode);
+        $allowed_extension = array('jpg', 'png', 'jpeg', 'gif');
+
+        if(in_array($extension, $allowed_extension)){
+            if($uploded_file['size'] <= 500000){
+
+                $insert = "INSERT INTO users (name, email, password, gender) VALUES ('$name', '$email', '$password', '$gender')";
+                $insert_result = mysqli_query($db_connection, $insert);
+
+                $insert_id = mysqli_insert_id($db_connection);
+                $file_name = $insert_id.'.'.$extension;
+                $new_location = "uploads/users/".$file_name;
+
+                move_uploaded_file($uploded_file['tmp_name'], $new_location);
+
+                $update = "UPDATE users SET photo='$file_name' WHERE id=$insert_id";
+                $update_result = mysqli_query($db_connection, $update);
+
+                $success = 'Successfully Registered !!!';
+                header('location:register.php?success='.$success);
+
+
+            }
+
+            else{
+                echo 'This file size is big';
+            }
+
+        }
+
+        else{
+            echo 'This Extension is not allowed';
+        }
+
+
+
+
+
+    }
+
+
+?>
---
+++ Class-13 review/users.php
@@ -0,0 +1,92 @@
+<?php
+
+    require 'header.php';
+    require 'db.php';
+
+?>
+
+<?php
+
+    $select = "SELECT * FROM users ORDER by id DESC";
+    $select_result = mysqli_query($db_connection, $select);
+
+?>
+
+<section>
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-12">
+                <div class="header text-center text-white bg-secondary py-2">
+                    <h3>Users Information</h3>
+                </div>
+                <table class="table table-dark table-striped">
+
+                    <tr>
+                        <th>Id</th>
+                        <th>NAME</th>
+                        <th>EMAIL</th>
+                        <th>PASSWORD</th>
+                        <th>GENDER</th>
+                        <th>PHOTO</th>
+                        <th>CREATED TIME</th>
+                        <th class="text-center">ACTION</th>
+                    </tr>
+
+                <?php foreach($select_result as $user_info) {  ?>
+
+                    <tr>
+                        <td><?php echo $user_info['id']; ?></td>
+                        <td><?php echo $user_info['name']; ?></td>
+                        <td><?php echo $user_info['email']; ?></td>
+                        <td><?php echo "********"; ?></td>
+                        <td><?php echo $user_info['gender']; ?></td>
+                        <td><img src="uploads/users/<?php echo $user_info['photo']; ?>" alt="Photo" width="50"></td>
+                        <td><?php echo $user_info['created_at']; ?></td>
+                        <td class="text-center">
+                            <a href="profile.php?id=<?php echo $user_info['id']; ?>" class="btn btn-success">View</a>
+                            <a href="edit_user.php?id=<?php echo $user_info['id']; ?>" class="btn btn-warning">Edit</a>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $user_info['id']; ?>">
+                                Delete
+                            </button>
+         
+                        
+                        </td>
+
+                    </tr>
+
+                    
+                    <div class="modal fade" id="exampleModal<?php echo $user_info['id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Are you sure to Delete ??</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                                Deleted file can not be recover  !!!
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                            <a href="delete_user.php?id=<?php echo $user_info['id']; ?>" class="btn btn-danger">Yes</a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                
+                <?php } ?>
+
+                </table>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php
+
+    require 'footer.php';
+
+?>
