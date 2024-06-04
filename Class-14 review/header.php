a5ff4690fb3544b8c41f9bbd6f2eeb2123c0a6f2
Raisul-19 <raisul15-14119@diu.edu.bd>
2024-06-04 20:48:29 +0600

    class 14 review

---
+++ Class-14 review/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in Form</title>
+
+    <!-- bootstrap css file link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+        .pass-btn{
+            position: relative;
+        }
+        .pass-btn button{
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    </style>
+
+</head>
+<body>
---
+++ Class-14 review/register-post.php
@@ -0,0 +1,139 @@
+<?php
+
+    require 'database.php'
+
+?>
+<?php
+
+    // print_r($_POST);
+    // echo "<br>";
+
+    // $name = $_POST['fname']."<br>".$_POST['email'];
+    
+    // echo $name;
+
+    $name = $_POST['fname'];
+    $email = $_POST['email'];
+
+    // $regex = 'regex value from google';
+
+    $pass = $_POST['password'];
+
+    $upper = preg_match('@[A-Z]@', $pass);
+    $lower = preg_match('@[a-z]@', $pass);
+    $number = preg_match('@[0-9]@', $pass);
+    $splcr = preg_match('@[#,$,,,&,*]@', $pass);
+
+    $rpass = $_POST['re-password'];
+
+    $gender = $_POST['gender'];
+    $photo = $_FILES['photo'];
+    
+
+    $select = "SELECT COUNT(*) as exist_email FROM users WHERE email=$email";
+    $select_result = mysqli_query($db_connection, $select);
+    $after_assoc = mysqli_fetch_assoc($select_result);
+
+
+
+
+    
+    if(empty($name)){
+        $err_msg = "nam lekh beta !";
+        header('location:register.php?nam_err='.$err_msg);
+    }
+    else if(empty($email)){
+        $err_msg = "email de beta !";
+        header('location:register.php?eml_err='.$err_msg);
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $err_msg = "thik thak email de beta !";
+        header('location:register.php?eml_err='.$err_msg);
+
+    }
+
+    // else if(!preg_match($regex, $email)){
+    //     $err_msg = "thik thak email de beta !";
+    //     header('location:index.php?eml_err='.$err_msg);
+    // }
+
+    else if(empty($pass)){
+        $err_msg = "gupon number de !";
+        header('location:register.php?pass_err='.$err_msg);
+    }
+    else if(!$upper || !$lower || !$number || !$splcr || strlen($pass) < 8){
+        $err_msg = "soktisali gupon number de !";
+        header('location:register.php?pass_err='.$err_msg);
+    }
+    else if(empty($rpass)){
+        $err_msg = "gupon number de !";
+        header('location:register.php?rpass_err='.$err_msg);
+    }
+    else if($pass != $rpass){
+        $err_msg = " vul gupon number disos !";
+        header('location:register.php?rpass_err='.$err_msg);
+    }
+    else if(empty($gender)){
+        $err_msg = "pola na maiya heida ko ?";
+        header('location:register.php?gender_err='.$err_msg);
+    }
+
+
+    else{
+
+        $uploaded_file = $_FILES['photo'];
+        $after_explod = explode('.', $uploaded_file['name']);
+        $extension = end($after_explod);
+        $allowed_extension = array('jpg','jpeg','png','gif');
+
+        if(in_array($extension, $allowed_extension)){
+            if($uploaded_file['size'] <= 50000000){
+
+                if($after_assoc['exist_email']==1){
+                    $_SESSION['exist'] = 'This Email Already Exist';
+                    header('location:register.php');
+                }
+                else{
+                    $insert = "INSERT INTO users (name, email, password, gender) VALUES ('$name', '$email', '$pass', '$gender')";
+                    $insert_result = mysqli_query($connection, $insert);
+    
+                    $user_id = mysqli_insert_id($connection);
+                    $file_name = $user_id.'.'.$extension;
+                    $new_location = "upload/user/".$file_name;
+    
+                    move_uploaded_file($uploaded_file['tmp_name'], $new_location);
+    
+                    $update = "UPDATE users SET photo='$file_name' WHERE id=$user_id";
+                    $update_result = mysqli_query($connection, $update);
+    
+    
+                    $success = "Successfully Registered !!!";
+                    header('location:register.php?succ_msg='.$success);
+                }
+
+
+            }
+            else{
+                echo "File size is bigger";
+
+                $success = "Failed to submit !!!";
+                header('location:register.php?succ_msg='.$success);
+            }
+            
+
+        }
+        else{
+            echo "This file extention not allowe";
+            $success = "Failed to submit !!!";
+            header('location:register.php?succ_msg='.$success);
+        }
+
+
+
+
+
+
+    }
+
+
+?>
